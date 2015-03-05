ffcd830a0f7c0661b5f038298ba79dcdc7a7fead
Alexandre Marones <alexandre.marones@m4u.com.br>
2015-03-05 03:55:28 -0300

    fixing brazilian edit text

--- Brazilian-Commons/library/src/main/java/br/com/m4u/commons/brazilian/library/components/editext/BrazilianEditText.java
+++ Brazilian-Commons/library/src/main/java/br/com/m4u/commons/brazilian/library/components/editext/BrazilianEditText.java
@@ -1,4 +1,4 @@
-package br.com.m4u.commons.brazilian.library.editext;
+package br.com.m4u.commons.brazilian.library.components.editext;
 
 import android.content.Context;
 import android.util.AttributeSet;
